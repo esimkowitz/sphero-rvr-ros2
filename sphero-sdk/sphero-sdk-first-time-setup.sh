be2bfc0ad6d06d53df35516803453fc26ac60501
Evan Simkowitz <esimkowitz@wustl.edu>
2022-10-02 02:15:13 -0700

    fix complete path for first-time-setup script

--- sphero-sdk/sphero-sdk-first-time-setup.sh
+++ sphero-sdk/sphero-sdk-first-time-setup.sh
@@ -2,8 +2,9 @@
 
 # This script guides a user through setting up the Sphero RVR Python SDK.
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Install the SDK dependencies (only for the current user)
-pip3 install --user -r requirements.txt
+pip3 install --user -r $SCRIPT_DIR/sphero-sdk-raspberry-python/requirements.txt
 
 # Reload ~/.profile.  In recent Raspberry Pi OS releases, this automatically includes $HOME/.local/bin in the path once it exists.
 source ~/.profile
