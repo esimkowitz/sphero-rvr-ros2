a038ee766f5f1788dfda479190eefe7e911e8035
Evan Simkowitz <esimkowitz@wustl.edu>
2023-05-05 23:41:30 -0700

    try running in ros entrypoint

--- scripts/container/ros_entrypoint.sh
+++ scripts/container/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 source /opt/ros/humble/setup.bash
 source /app/ros2_ws/install/setup.bash
 
+python3 /app/sphero-sdk/sphero-sdk-raspberry-python/getting_started/observer/leds/set_all_leds.py
+
 exec "$@"
