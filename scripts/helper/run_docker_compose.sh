1d1cf31a323975085934e916b58a696126f0f922
Evan Simkowitz <esimkowitz@wustl.edu>
2023-05-06 15:37:52 -0700

    fancy shiny script

--- scripts/helper/run_docker_compose.sh
+++ scripts/helper/run_docker_compose.sh
@@ -1,1 +1,43 @@
-sudo docker compose up
+#!/bin/bash
+
+# start aliases for pushd/popd so they don't echo to console
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+# end pushd/popd aliases
+
+# By default, we do not want to mock the RVR interface
+mock_rvr_val="false"
+
+# Start process option flags if present
+while getopts "bm" flag
+do
+    case "${flag}" in
+        b) 
+            echo "Running docker compose with optional build flag"
+            shouldbuild=true
+            ;;
+        m) 
+            echo "Mocking the RVR interface"
+            mock_rvr_val="true"
+            ;;
+    esac
+done
+# End process option flags
+
+# Get the root directory for the git repo
+reporoot=$(git rev-parse --show-toplevel)
+
+# start docker compose, should run from the repo root directory, then return to the pwd on exit
+pushd $reporoot
+if $shouldbuild; then
+    trap "MOCK_RVR=$mock_rvr_val docker compose up --build" EXIT
+else
+    trap "MOCK_RVR=$mock_rvr_val docker compose up" EXIT
+fi
+popd
+# end docker compose
